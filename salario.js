6e907ea44aa57b3f0ba3522829a2c4a3e23120fd
FmDeveloper <fmdeveloper2@gmail.com>
2025-04-01 16:23:30 -0300

    Initial commit

---
+++ salario.js
@@ -0,0 +1,82 @@
+import input from 'readline-sync';
+
+// Dados do salário mínimo e IPCA
+const salarios = [
+  {ano: 2010, salario: 510.00},
+  {ano: 2011, salario: 545.00},
+  {ano: 2012, salario: 622.00},
+  {ano: 2013, salario: 678.00},
+  {ano: 2014, salario: 724.00},
+  {ano: 2015, salario: 788.00},
+  {ano: 2016, salario: 880.00},
+  {ano: 2017, salario: 937.00},
+  {ano: 2018, salario: 954.00},
+  {ano: 2019, salario: 998.00},
+  {ano: 2020, salario: 1045.00},
+]
+const ipca = [
+  {ano: 2010, ipca: 5.91},
+  {ano: 2011, ipca: 6.50},
+  {ano: 2012, ipca: 6.08},
+  {ano: 2013, ipca: 6.30},
+  {ano: 2014, ipca: 6.32},
+  {ano: 2015, ipca: 9.28},
+  {ano: 2016, ipca: 6.29},
+  {ano: 2017, ipca: 3.46},
+  {ano: 2018, ipca: 3.75},
+  {ano: 2019, ipca: 3.73},
+  {ano: 2020, ipca: 3.20},
+]
+
+
+function listarOpcaoSelecionada() {
+  console.log('Bem-vindo ao projeto Salário x Inflação!');
+  console.log('Este projeto tem como objetivo comparar o crescimento do salário mínimo com a inflação (IPCA) ao longo dos anos.');
+  console.log('1 - Listar salários mínimos');
+  console.log('2 - Listar IPCA');
+  console.log('3 - Comparar salário mínimo com IPCA');
+  console.log('4 - Sair');
+  console.log('Selecione uma opção: ');
+  const opcao = input.questionInt('Opcao: ');
+  if (opcao === 1) {
+    listarSalarios();
+  } else if (opcao === 2) {
+    listarIPCA();
+  } else if (opcao === 3) {
+    compararSalarioComIPCA();
+  } else if (opcao === 4) {
+    console.log('Saindo...');
+  } else {
+    console.log('Opção inválida. Tente novamente.');
+    listarOpcaoSelecionada();
+  }
+
+  function listarSalarios(){
+    console.log('salarios minimos');
+    for(let i = 0; i < salarios.length; i++){
+      console.log(`Ano: ${salarios[i].ano}, Salário Mínimo: R$ ${salarios[i].salario}`);
+    }
+  }
+
+  function listarIPCA(){
+    console.log('IPCA');
+    for(let i = 0; i < ipca.length; i++){
+      console.log(`Ano: ${ipca[i].ano}, IPCA: ${ipca[i].ipca}%`);
+    }
+  }
+  function compararSalarioComIPCA(){
+    console.log('Comparação entre Salário Mínimo e IPCA');
+    for(let i = 0; i < salarios.length; i++){
+      const ano = salarios[i].ano;
+      const salario = salarios[i].salario;
+      const ipcaAno = ipca.find(item => item.ano === ano);
+      if (ipcaAno) {
+        const ipca = ipcaAno.ipca;
+        console.log(`Ano: ${ano}, Salário Mínimo: R$ ${salario}, IPCA: ${ipca}%`);
+      }
+    }
+  }
+
+}
+
+listarOpcaoSelecionada();
